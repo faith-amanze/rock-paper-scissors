3ce34befad711ef9e11eb4dd4dc80da0ad717371
Faith-Amanze <faith.amanze@gmail.com>
2024-03-04 16:32:41 +0100

    adjusted styles and script files

--- script.js
+++ script.js
@@ -1,56 +1,61 @@
-function trans(input){
-    switch(input){
-        case "rook": return 1;
-        case "paper": return 2;
-        case "scissor": return 3;
-        default: return -1;
+// Function to get computer's choice randomly
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+// Function to play a single round
+function playRound(playerSelection, computerSelection) {
+    // Convert playerSelection to lowercase for case-insensitivity
+    playerSelection = playerSelection.toLowerCase();
+
+    // Determine the winner or tie
+    if (
+        (playerSelection === 'rock' && computerSelection === 'Scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'Rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'Paper')
+    ) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'Paper') ||
+        (playerSelection === 'paper' && computerSelection === 'Scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'Rock')
+    ) {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+        return 'It\'s a Tie!';
     }
 }
 
-function result(playerInput){
-    if(playerInput == -1) {
-        console.log("That option is not available!!!");
-        console.log("Game Over!");
-        return;
+// Function to play a five-round game
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt('Enter your choice: Rock, Paper, or Scissors');
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+
+        // Update scores
+        const result = playRound(playerSelection, computerSelection);
+        if (result.includes('Win')) {
+            playerScore++;
+        } else if (result.includes('Lose')) {
+            computerScore++;
+        }
     }
 
-    const computerResult = (Math.floor(Math.random()*3)+1);
-
-    const check = trans(playerInput.toLowerCase()) - computerResult;
-
-    if(check == 0){
-        console.log("Draw!");
-    }
-
-    if( check == 1 || check == -2){
-        console.log("You Win!!");
-    }
-
-    if( check == -1 || check == 2){
-        console.log("You Lose");
-    }
-
-    let computerAns = "";
-
-    switch(computerResult){
-        case 1: {computerAns = "rook"; break;}
-        case 2: {computerAns = "paper"; break;}
-        case 3: {computerAns = "scissor"; break;}
-        default: return -1;
-    }
-
-    console.log("The computer result is: " + computerAns);
-}
-
-
-
-let playerInput;
-
-while(true){
-    playerInput = prompt("Rook, paper or scissor?");
-    result(playerInput);
-    let again = prompt("Do you want to play again?(Y = yes/other = no)");
-    if(again != 'Y'){
-        break;
+    // Determine the winner of the game
+    if (playerScore > computerScore) {
+        console.log('Congratulations! You win the game!');
+    } else if (playerScore < computerScore) {
+        console.log('Sorry, you lose the game.');
+    } else {
+        console.log('It\'s a tie game!');
     }
 }
+
+// Start the game
+playGame();
